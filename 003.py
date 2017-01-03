0f3bc60857956b8644afccd34dd5f1ed3f685797
Besa Demiri <besa.demiri@rwth-aachen.de>
2017-01-03 22:46:22 +0100

    bugfixes! (i always wanted to put that in a commit message!)

--- 003.py
+++ 003.py
@@ -1,14 +1,23 @@
 # Largest prime factor of 600851475143
 
-n = 600851475143
-i = 3
-k = 2
+# Hilfsfunktion to check if prime
+def checkPrime(a):
+    for b in range (2,a):
+        if a % b == 0:
+       	    return False
+    else:    
+        return True
 
-while i < 600851475143:
-    for j in range (2,i):
-        if (i % j) == 0:
-            i = i + 2
-        else:
-            k = i
-            i = i + 2
-print (k)
+n = 600851475143	# Number whose largest prime factor is to be determined
+i = 3				# Possible highest prime factor getting checked in the function
+k = 2				# Current highest prime factor
+
+while i <= n:							# Do this as long as i is a possible prime factor
+    if n % i == 0 and checkPrime(i):	# Is i a factor of n and a prime?
+       	k = i							# Set current highest prime factor to i
+       	n = n / i						# Divide n by found prime factor
+    i = i + 2							# Increment i
+print (k)								# Print the last saved value of i 
+
+
+
---
+++ checkprime.py
@@ -0,0 +1,8 @@
+a = 8
+
+for b in range (2,a):
+    if a % b == 0:
+   	    print ("not a prime")
+   	    break
+else:
+    print ("prime")
