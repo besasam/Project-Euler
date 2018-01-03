a2b27bd6dc4cab06b06fe3d4e2b7920a0d2f0097
0xBE5A <besademiri@ymail.com>
2018-01-03 15:04:58 +0100

    Update 005.py

--- 005.py
+++ 005.py
@@ -1,17 +1,16 @@
 # Smallest positive number that is divisible by all of the numbers from 1 to 20
 
-n = 20
-a = False
-i = 1
-while 1:
-    for i <= 20:
-        if n % i == 0:
-            a = True
-            i = i + 1
-        else:
-            a = False
-            ???
-    if a == True:
-        print (n)
-        break
-    n = n + 1
+def smallestMultiple(x):
+    res = 0
+    i = x
+    while res==0:
+        flag = True
+        for k in range(1, x+1):
+            if i%k!=0:
+                flag = False
+        if flag==True:
+            res = i
+        i = i + x
+    return res
+
+print smallestMultiple(20)
